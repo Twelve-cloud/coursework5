d757ff81963dfd403714a8dca8120ca956abf703
twelve <kana.suzucki@gmail.com>
2022-11-10 16:22:40 +0300

    feature: created User model and changed .env file with createsuperuser data

--- stocktrader/user/models.py
+++ stocktrader/user/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        verbose_name='Email',
+    )
+
+    image = models.URLField(
+        max_length=200,
+        null=True,
+        blank=True,
+        verbose_name='Image',
+    )
+
+    is_blocked = models.BooleanField(
+        default=False,
+        verbose_name='Blocked?',
+    )
+
+    is_active = models.BooleanField(
+        default=False,
+        verbose_name='Active?'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'is_active']
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['-is_blocked', 'username']
+        db_table = 'User'
+
+    def __str__(self):
+        return f'{self.id}. {self.username}'
+
+    def get_absolute_url(self):
+        return f'/users/{self.pk}/'
