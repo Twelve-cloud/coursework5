811b3a9788f8549dab3d5cfb110b3c3dee74c148
twelve <kana.suzucki@gmail.com>
2022-11-13 19:58:47 +0300

    refactor: some changes

--- stocktrader/user/views.py
+++
@@ -1,8 +0,0 @@
-from user.serializers import UserSerializer
-from rest_framework import viewsets
-from user.models import User
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
