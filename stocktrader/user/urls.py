052f802ae0268ae0ea1a41be240fc9e2c7baf888
twelve <kana.suzucki@gmail.com>
2022-11-13 19:33:26 +0300

    refactor: change basename for user url

--- stocktrader/user/urls.py
+++ stocktrader/user/urls.py
@@ -3,6 +3,6 @@ from user import views
 
 
 router = DefaultRouter()
-router.register(prefix='users', viewset=views.UserViewSet, basename='users')
+router.register(prefix='users', viewset=views.UserViewSet, basename='user')
 
 urlpatterns = router.urls
