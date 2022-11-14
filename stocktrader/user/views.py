1154bdce1aff11e74d5b7f6574df1ab04ab0d44c
twelve <kana.suzucki@gmail.com>
2022-11-14 16:34:27 +0300

    feature: set permissions to crud

--- stocktrader/user/views.py
+++ stocktrader/user/views.py
@@ -1,3 +1,5 @@
+from user.permissions import IsUserOwner, IsNotAuthentificatedOrAdmin
+from rest_framework.permissions import IsAuthenticated
 from user.serializers import UserSerializer
 from rest_framework import viewsets
 from user.models import User
@@ -6,3 +8,30 @@ from user.models import User
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_map = {
+        'create': (
+            IsNotAuthentificatedOrAdmin,
+        ),
+        'list': (
+            IsAuthenticated,
+        ),
+        'retrieve': (
+            IsAuthenticated,
+        ),
+        'update': (
+            IsAuthenticated,
+            IsUserOwner,
+        ),
+        'partial_update': (
+            IsAuthenticated,
+            IsUserOwner,
+        ),
+        'destroy': (
+            IsAuthenticated,
+            IsUserOwner,
+        )
+    }
+
+    def get_permissions(self):
+        self.permission_classes = self.permission_map.get(self.action, [])
+        return super(self.__class__, self).get_permissions()
