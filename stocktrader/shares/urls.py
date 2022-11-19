9064f53f9534867f7024c3975c21840cae21ef06
twelve <kana.suzucki@gmail.com>
2022-11-19 16:00:01 +0300

    chore: added logic to account create and account update

--- stocktrader/shares/urls.py
+++ stocktrader/shares/urls.py
@@ -4,10 +4,30 @@ from shares import views
 
 router = DefaultRouter()
 
-router.register(prefix='brokers', viewset=views.BrokerViewSet, basename='broker')
-router.register(prefix='orders', viewset=views.OrderViewSet, basename='order')
-router.register(prefix='accounts', viewset=views.AccountViewSet, basename='account')
-router.register(prefix='stocks', viewset=views.AccountViewSet, basename='stock')
-router.register(prefix='accounts-history', viewset=views.AccountViewSet, basename='account_history')
+router.register(
+    prefix='brokers',
+    viewset=views.BrokerViewSet,
+    basename='broker'
+)
+router.register(
+    prefix='orders',
+    viewset=views.OrderViewSet,
+    basename='order'
+)
+router.register(
+    prefix='accounts',
+    viewset=views.AccountViewSet,
+    basename='account'
+)
+router.register(
+    prefix='stocks',
+    viewset=views.StockViewSet,
+    basename='stock'
+)
+router.register(
+    prefix='accounts-history',
+    viewset=views.AccountHistoryViewSet,
+    basename='account_history'
+)
 
 urlpatterns = router.urls
--- stocktrader/shares/views.py
+++ stocktrader/shares/views.py
@@ -8,7 +8,9 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from django.contrib.auth.models import AnonymousUser
+from rest_framework.response import Response
 from rest_framework import viewsets, mixins
+from rest_framework import status
 
 
 class BrokerViewSet(viewsets.ModelViewSet):
@@ -86,11 +88,7 @@ class OrderViewSet(mixins.CreateModelMixin,
             return user.orders.all()
 
 
-class AccountViewSet(mixins.CreateModelMixin,
-                     mixins.RetrieveModelMixin,
-                     mixins.DestroyModelMixin,
-                     mixins.ListModelMixin,
-                     viewsets.GenericViewSet):
+class AccountViewSet(viewsets.ModelViewSet):
     serializer_class = AccountSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['balance', 'balance_with_shares']
@@ -105,6 +103,14 @@ class AccountViewSet(mixins.CreateModelMixin,
         ),
         'retrieve': (
             IsAuthenticated,
+        ),
+        'update': (
+            IsAuthenticated,
+            IsAdminUser
+        ),
+        'partial_update': (
+            IsAuthenticated,
+            IsAdminUser
         ),
         'destroy': (
             IsAuthenticated,
@@ -123,8 +129,49 @@ class AccountViewSet(mixins.CreateModelMixin,
         else:
             return user.accounts.all()
 
+    def create(self, request, *args, **kwargs):
+        if len(request.user.accounts.all()) == 3:
+            return Response(
+                data='Error: Number of accounts must be less than 3',
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
-class StockViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin):
+        broker, user = request.data['broker'], request.data['user']
+
+        if Account.objects.filter(broker=broker, user=user).exists():
+            return Response(
+                data='Error: Number of accounts in one broker must be single',
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        response = super().create(request, *args, **kwargs)
+
+        AccountHistory.objects.create(
+            account=Account.objects.get(broker=broker, user=user),
+            balance=request.data['balance'],
+            balance_with_shares=request.data['balance_with_shares']
+        )
+
+        return response
+
+    def update(self, request, *args, **kwargs):
+        account = Account.objects.get(pk=kwargs.get('pk'))
+
+        AccountHistory.objects.create(
+            account=account,
+            balance=request.data.get('balance', account.balance),
+            balance_with_shares=request.data.get(
+                'balance_with_shares',
+                account.balance_with_shares
+            )
+        )
+
+        return super().update(request, *args, **kwargs)
+
+
+class StockViewSet(mixins.RetrieveModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
@@ -146,17 +193,23 @@ class StockViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin):
 
     def get_queryset(self):
         user = self.request.user
-        for account in user.accounts.all():
-            print(account.shares, type(account.shares))
+        accounts = user.accounts.all()
+        if accounts:
+            shares = accounts[0].shares.all()
+            for account in accounts:
+                shares |= account.shares.all()
+            return shares
 
 
-class AccountHistoryViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin):
+class AccountHistoryViewSet(mixins.RetrieveModelMixin,
+                            mixins.ListModelMixin,
+                            viewsets.GenericViewSet):
     queryset = AccountHistory.objects.all()
     serializer_class = AccountHistorySerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    filterset_fields = ['name', 'type']
-    search_fields = ['name', 'type']
-    ordering_fields = ['name', 'type', 'rate']
+    filterset_fields = ['balance', 'balance_with_shares']
+    search_fields = ['balance', 'balance_with_shares']
+    ordering_fields = ['balance', 'balance_with_shares', 'date']
     permission_map = {
         'list': (
             IsAuthenticated,
@@ -172,5 +225,9 @@ class AccountHistoryViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin):
 
     def get_queryset(self):
         user = self.request.user
-        for account in user.accounts.all():
-            print(account.shares, type(account.shares))
+        accounts = user.accounts.all()
+        if accounts:
+            history = accounts[0].history.all()
+            for account in accounts:
+                history |= account.history.all()
+            return history
