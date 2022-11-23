4848bd8dde33306c76cc658d340806005a7c6c3d
twelve <kana.suzucki@gmail.com>
2022-11-24 02:25:25 +0300

    feature: created user admin panel

--- stocktrader/user/admin.py
+++ stocktrader/user/admin.py
@@ -1,3 +1,13 @@
-# from django.contrib import admin
+from django.contrib import admin
+from user.models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    list_display_links = ('email', 'username')
+    search_fields = ('email', 'username')
+    list_filter = ('email', 'username')
+    ordering = ('email', 'username')
+
+
+admin.site.register(User, UserAdmin)
